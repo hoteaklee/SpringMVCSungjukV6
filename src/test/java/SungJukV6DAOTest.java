c8e43b5340e905387f6a522be753cda41f4de525
hoteaklee <cc18ht@naver.com>
2023-04-27 15:04:46 +0900

    스프링MVC 프로젝트 - 단위테스트 수행 2

--- src/test/java/SungJukV6DAOTest.java
+++ src/test/java/SungJukV6DAOTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +9,16 @@ import riho.spring4.sungjukv6.model.SungJukVO;
 
 import java.util.List;
 
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+
 //단위unit 테스트
 //하나의 모듈을 기준으로 독립적으로 진행하는 가장 작은 단위의 테스트
 //모듈은 애플리케이션에서 작동하는 하나의 기능 또는 메소드를 의미
 //애플리케이션을 구성하는 하나의 기능이 올바르게 동작하는지를 독립적으로 테스트하는 것
 //"어떤 기능이 실행되면 어떤 결과가 나온다" 수준으로 테스트 함
+// 테스트 결과 확인은 assertXxx 메서드를 이용함
+// => ~equals, ~notequals, ~null, ~notnull, ~true, ~false
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:root-context.xml","classpath:servlet-context.xml"})
@@ -24,7 +30,9 @@ public class SungJukV6DAOTest {
     public void test1(){
         System.out.println("~~ Test 1 시작 ~~");
         List<SungJukVO> sjs = sjdao.selectSungJuk();
-        System.out.println(sjs);
+        //System.out.println(sjs);
+        assertNotNull(sjs);
+
     }
 
     @Test //테스트 슈트
@@ -32,7 +40,8 @@ public class SungJukV6DAOTest {
         System.out.println("~~ Test 2 시작 ~~");
         int sjno = 4;
         SungJukVO sj = sjdao.seletOneSungJuk(sjno);
-        System.out.println(sj);
+       //System.out.println(sj);
+        assertNotNull(sj);
     }
 
     @Test //테스트 슈트
@@ -41,7 +50,8 @@ public class SungJukV6DAOTest {
         SungJukVO sj = new SungJukVO("a", 99,99,99);
         sj.setTot(297); sj.setAvg(99.0); sj.setGrd('수');
         int cnt = sjdao.insertSungJuk(sj);
-        System.out.println(cnt);
+        //System.out.println(cnt);
+        assertEquals(1,cnt);
     }
 
 }
---
+++ src/test/java/SungJukV6ServiceTest.java
@@ -0,0 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+import riho.spring4.sungjukv6.model.SungJukVO;
+import riho.spring4.sungjukv6.service.SungJukV6Service;
+import riho.spring4.sungjukv6.service.SungJukV6ServiceImpl;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class SungJukV6ServiceTest {
+    SungJukV6Service sjsrv;
+    @Before
+    public void setup(){
+      sjsrv  = new SungJukV6ServiceImpl(null);
+    }
+
+
+    @Test
+    public void test1(){
+        SungJukVO sj = new SungJukVO("test1", 99,99,99);
+
+        sjsrv.computeSungJuk(sj);
+
+        assertEquals(297,sj.getTot());
+        assertEquals(99,sj.getAvg(),0.0);
+        assertEquals('수',sj.getGrd());
+    }
+}
